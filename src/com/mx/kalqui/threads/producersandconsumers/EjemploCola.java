e25cb7d3a8933e8cac9c1235964db5ed264697d2
Karina Alquicira <alquicirakarina2@gmail.com>
2026-08-10 22:55:23 -0600

    Se crearon nuevas clases de ejemplo.

--- src/com/mx/kalqui/threads/producersandconsumers/EjemploCola.java
+++ src/com/mx/kalqui/threads/producersandconsumers/EjemploCola.java
@@ -3,8 +3,8 @@
  */
 package com.mx.kalqui.threads.producersandconsumers;
 
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingDeque;
 
 /**
  * @author Karina
@@ -14,17 +14,19 @@ public class EjemploCola {
 
 	public static void main(String[] args) {
 
+		int capacity = 5;
 		// Se define una cola con capacidad máxima de 3 elementos
-		BlockingQueue<Integer> cola = new LinkedBlockingDeque<Integer>(3);
+		BlockingQueue<Integer> cola = new ArrayBlockingQueue<>(capacity);
 
 		// Hilo Productor
 		Thread productor = new Thread(() -> {
 			try {
-				for (int i = 0; i <= 5; i++) {
+				for (int i = 0; i < 10; i++) {
+					System.out.println("Producer adding: " + i);
 					cola.put(i); // Bloquea el hilo si la cola se llena.
-					System.out.println("Producido: " + i);
-					Thread.sleep(200);
+					Thread.sleep(100);
 				}
+				cola.put(-1);
 			} catch (InterruptedException e) {
 				Thread.currentThread().interrupt();
 			}
@@ -33,11 +35,17 @@ public class EjemploCola {
 		// Hilo consumidor
 		Thread consumidor = new Thread(() -> {
 			try {
-				for (int i = 0; i <= 5; i++) {
-					Integer valor = cola.take();// Bloquea el hilo si la cola está vacía.
-					System.out.println("Consumido: " + valor);
-					Thread.sleep(800); // EL consumidor es màs lento deliberadamente.
+				int value;
+				while ((value = cola.take()) != -1) {
+					System.out.println("Consumer consumed: " + value);
+					Thread.sleep(200);
 				}
+				System.out.println("Consumer finished.");
+//				for (int i = 0; i <= 5; i++) {
+//					Integer valor = cola.take();// Bloquea el hilo si la cola está vacía.
+//					System.out.println("Consumido: " + valor);
+//					Thread.sleep(800); // EL consumidor es màs lento deliberadamente.
+//				}
 			} catch (InterruptedException e) {
 				Thread.currentThread().interrupt();
 			}
